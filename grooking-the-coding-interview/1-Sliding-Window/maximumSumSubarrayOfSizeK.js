52a7446918578d5a3e62153d38780a8c39baecef
unknown <diegollanespasos@hotmail.com>
2020-10-01 23:46:46 -0500

    Maximum Sum Subarray of size K

---
+++ grooking-the-coding-interview/1-Sliding-Window/maximumSumSubarrayOfSizeK.js
@@ -0,0 +1,29 @@
+// Maximum Sum Subarray of Size K
+
+/* Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+
+Example 1:
+Input: [2, 1, 5, 1, 3, 2], k=3 
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+
+Example 2:
+Input: [2, 3, 4, 1, 5], k=2 
+Output: 7
+Explanation: Subarray with maximum sum is [3, 4].*/
+
+function maximumSum(k, array){
+	let maxSum =  Number.MIN_VALUE;
+	let sum = 0;
+
+	for(let i = 0; i < array.length; i++){
+		sum += array[i];
+		if(i >= k-1){
+			if(sum > maxSum){
+				maxSum = sum;
+			}
+			sum = sum - array[i -(k - 1)];
+		}
+	}
+	return maxSum;
+}
