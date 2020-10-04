727b7c88e667e74ed66b762510eff5cf724ca9ec
unknown <diegollanespasos@hotmail.com>
2020-10-03 20:03:52 -0500

    Pair with a Target Sum

---
+++ grooking-the-coding-interview/2-Two-Pointers/pairWithATargetSum.js
@@ -0,0 +1,27 @@
+// PAIR WITH A TARGET SUM
+/* Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+
+Example 1:
+Input: [1, 2, 3, 4, 6], target=6
+Output: [1, 3]
+Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+
+Example 2:
+Input: [2, 5, 9, 11], target=11
+Output: [0, 2]
+Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11 */
+
+function pairWithTargetSum(array, target){
+	let leftPointer = 0;
+	let rightPointer = array.length-1;
+
+	while(leftPointer < rightPointer){
+		let sum = array[leftPointer] + array[rightPointer];
+
+		if(sum === target) return [leftPointer,rightPointer];
+		else if(sum > target) rightPointer--;
+		else leftPointer++;
+	}
+	return -1;
+}
